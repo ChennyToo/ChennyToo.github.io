271ad24727cd782954df055356c3b753d58cc7d9
chenk24 <chenk24@hsnet.ahsd.org>
2024-11-25 01:24:49 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Calender from './Components/MainBodyComponents/Calendar';
+import Header from './Components/Header';
+import MainBody from './Components/MainBody';
+
+function App() {
+  return (
+    <div style={{overflow: 'hidden', height: '100vh'}}>
+
+    <Header></Header>
+    <MainBody></MainBody>
+    {/* <div>hi</div>
+    <Calender></Calender> */}
+
+
+
+</div>
+  );
+}
+
+export default App;
